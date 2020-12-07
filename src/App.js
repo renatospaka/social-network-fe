d6850b393bdf0feb8f27c0f329c2f4962e6024ef
Renato Spakauskas <renatospaka@gmail.com>
2020-12-07 16:58:12 -0300

    Signing users up

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
   state = {
     showBackdrop: false,
     showMobileNav: false,
-    isAuth: true,
+    isAuth: false,
     token: null,
     userId: null,
     authLoading: false,
@@ -100,7 +100,17 @@ class App extends Component {
   signupHandler = (event, authData) => {
     event.preventDefault();
     this.setState({ authLoading: true });
-    fetch('URL')
+    fetch('http://localhost:8080/auth/signup', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: authData.signupForm.email.value,
+        password: authData.signupForm.password.value,
+        name: authData.signupForm.name.value
+      })
+    })
       .then(res => {
         if (res.status === 422) {
           throw new Error(
@@ -109,7 +119,7 @@ class App extends Component {
         }
         if (res.status !== 200 && res.status !== 201) {
           console.log('Error!');
-          throw new Error('Creating a user failed!');
+          throw new Error('Creating user failed!');
         }
         return res.json();
       })
